62b1c393683a0339469ed7e83957218c5b2f2e9d
Akilan T <techakilan@gmail.com>
2022-04-10 13:14:45 +0530

    hardcoded movie catalog api

---
+++ movie-catalog-service/src/main/java/com/westerly/moviecatalogservice/controllers/MovieCatalogController.java
@@ -0,0 +1,28 @@
+package com.westerly.moviecatalogservice.controllers;
+
+import com.westerly.moviecatalogservice.models.Movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogController {
+   
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${MOVIE_INFO_SERVICE}")
+    private String movieInforService;
+
+    @GetMapping("/{userId}/")
+    public Movie home() {
+        Movie movie = restTemplate.getForObject(
+            "http://"+movieInforService+"/movie/2", Movie.class);
+        return movie;
+    }
+}
