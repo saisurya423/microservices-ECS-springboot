1cbb9243846ddd3ac2baf27d06b7433f529c4340
Akilan T <techakilan@gmail.com>
2022-04-10 22:36:27 +0530

    update movie catalog service to get data from info and ratins service

--- movie-catalog-service/README.md
+++ movie-catalog-service/README.md
@@ -1,10 +1,11 @@
 export MOVIE_INFO_SERVICE="localhost:9091"
+export MOVIE_RATINGS_SERVICE="localhost:9092"
 
 ./mvnw clean package
 
 docker build -t westerly/demo-movie-catalog-service .
 
-docker run  westerly/demo-movie-catalog-service -e MOVIE_INFO_SERVICE="localhost:9091" -p 9090:9090
+docker run  -e MOVIE_INFO_SERVICE='54.187.221.177:9091' -p 9090:9090 westerly/demo-movie-catalog-service 
 
 docker images
 
--- movie-catalog-service/src/main/java/com/westerly/moviecatalogservice/controllers/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/westerly/moviecatalogservice/controllers/MovieCatalogController.java
@@ -1,10 +1,17 @@
 package com.westerly.moviecatalogservice.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.westerly.moviecatalogservice.models.CatalogItem;
 import com.westerly.moviecatalogservice.models.Movie;
+import com.westerly.moviecatalogservice.models.UserRating;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,12 +24,19 @@ public class MovieCatalogController {
     RestTemplate restTemplate;
 
     @Value("${MOVIE_INFO_SERVICE}")
-    private String movieInforService;
+    private String movieInfoService;
+    @Value("${MOVIE_RATINGS_SERVICE}")
+    private String movieRatingsService;
 
     @GetMapping("/{userId}")
-    public Movie home() {
-        Movie movie = restTemplate.getForObject(
-             "http://"+movieInforService+"/movie/2", Movie.class);
-        return movie;
-    }
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        UserRating userRating = restTemplate.getForObject("http://"+movieRatingsService+"/ratingsdata/user/" + userId,
+                UserRating.class);
+        return userRating.getRatings().stream().map(
+                rating -> {
+                    Movie movie = restTemplate.getForObject(
+                        "http://"+movieInfoService+"/movie/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+                }).collect(Collectors.toList());
+    }  
 }
